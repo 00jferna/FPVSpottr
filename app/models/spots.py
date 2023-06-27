f39b872c39cfc5f216cbd82646bfd35795487fd7
Jesse Fernandez <jfernandez@coderunner.studio>
2023-06-27 01:05:04 -0500

    Updating Enum types

--- app/models/spots.py
+++ app/models/spots.py
@@ -14,7 +14,7 @@ class Spot(db.Model):
     latitude = db.Column(db.Float(), nullable=False)
     longitude = db.Column(db.Float(), nullable=False)
     address = db.Column(db.String(255))
-    type = db.Column('type',db.Enum('field','park','playground','bando','industrialPark', name='type'), nullable=False)
+    type = db.Column('type', db.Enum('field','park','playground','bando','industrialPark', name='type'), nullable=False)
     owner = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     status = db.Column('status', db.Enum('field','park', name='status'), nullable=False)
     preview_img = db.Column(db.String(255), nullable=False)
