5f6c32e9981a749505c488ee0c1ae2330b397114
Jesse Fernandez <jfernandez@coderunner.studio>
2023-06-27 02:52:47 -0500

    Adding Seeders

---
+++ app/seeds/spots.py
@@ -0,0 +1,25 @@
+from app.models.spots import db, Spot, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_spots():
+    demo = Spot(
+        name='Demo', latitude='35.29846332258847', longitude='-89.65428151451766',type='park', owner=1,status='park')
+    db.session.add(demo)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_spots():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.spots RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM spots"))
+        
+    db.session.commit()
