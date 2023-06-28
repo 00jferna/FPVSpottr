250a4f07e1b20700323a8a8d9520bc36cde3ac40
Jesse Fernandez <jfernandez@coderunner.studio>
2023-06-27 22:21:01 -0500

    Removing postgresql dialect to migration

--- migrations/versions/20201120_150602_create_users_table.py
+++ migrations/versions/20201120_150602_create_users_table.py
@@ -20,6 +20,11 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+spot_type = sa.Enum('field','park','playground','bando','industrialPark', name='type')
+group_type = sa.Enum('Racing', 'Freestyle', 'Cinematic', 'Exploring', 'Tiny Whoop', name='type')
+spot_status = sa.Enum('field','park', name='status')
+member_privileges = sa.Enum('member','admin','owner', name='privileges')
+
 def upgrade():
     op.create_table('users',
                     sa.Column('id', sa.Integer(), nullable=False),
@@ -41,9 +46,9 @@ def upgrade():
                     sa.Column('latitude', sa.Float(), nullable=False),
                     sa.Column('longitude', sa.Float(), nullable=False),
                     sa.Column('address', sa.String(length=255)),
-                    sa.Column('type', postgresql.ENUM('field','park','playground','bando','industrialPark', name='type'), nullable=False),
+                    sa.Column('type', spot_type, nullable=False),
                     sa.Column('owner', sa.Integer(), nullable=False),
-                    sa.Column('status', postgresql.ENUM('field','park', name='status'), nullable=False),
+                    sa.Column('status', spot_status, nullable=False),
                     sa.Column('preview_img', sa.String(length=255), nullable=False),
                     sa.Column('created_at', sa.DateTime(), nullable=True),
                     sa.Column('updated_at', sa.DateTime(), nullable=True),
@@ -81,7 +86,7 @@ def upgrade():
                     sa.Column('name', sa.String(length=255), nullable=False),
                     sa.Column('desc', sa.String(length=255)),
                     sa.Column('visibility', sa.Boolean(), nullable=False),
-                    sa.Column('type', postgresql.ENUM('Racing', 'Freestyle', 'Cinematic', 'Exploring', 'Tiny Whoop', name='type'), nullable=False),
+                    sa.Column('type', group_type, nullable=False),
                     sa.Column('owner', sa.Integer(), nullable=False),
                     sa.Column('preview_img', sa.String(length=255), nullable=False),
                     sa.Column('created_at', sa.DateTime(), nullable=True),
@@ -132,7 +137,7 @@ def upgrade():
                     sa.Column('id', sa.Integer(), nullable=False),
                     sa.Column('member', sa.Integer(), nullable=False),
                     sa.Column('group_id', sa.Integer(), nullable=False),
-                    sa.Column('privileges', postgresql.ENUM('member','admin','owner', name='privileges'), nullable=False),
+                    sa.Column('privileges', member_privileges, nullable=False),
                     sa.Column('created_at', sa.DateTime(), nullable=True),
                     sa.Column('updated_at', sa.DateTime(), nullable=True),
                     sa.PrimaryKeyConstraint('id'),
@@ -155,10 +160,14 @@ def upgrade():
 def downgrade():
     op.drop_table('users')
     op.drop_table('spots')
+    spot_type.drop(op.get_bind(), checkfirst=False)
+    spot_status.drop(op.get_bind(), checkfirst=False)
     op.drop_table('reviews')
     op.drop_table('favorites')
     op.drop_table('groups')
+    group_type.drop(op.get_bind(), checkfirst=False)
     op.drop_table('images')
     op.drop_table('videos')
     op.drop_table('visits')
     op.drop_table('members')
+    member_privileges.drop(op.get_bind(), checkfirst=False)
